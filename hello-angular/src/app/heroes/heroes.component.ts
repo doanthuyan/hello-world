c84aeb8dd77df8bac81073552c91613e6784a9ee
doanthuyan <doanthuyan@gmail.com>
2020-05-26 11:18:47 +0700

    Fix the build

--- hello-angular/src/app/heroes/heroes.component.ts
+++ hello-angular/src/app/heroes/heroes.component.ts
@@ -18,30 +18,29 @@ export class HeroesComponent implements OnInit {
 	selectedHero: Hero;
 	myHeroList : Hero[];
 	
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+	constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  ngOnInit() {
-	this.getHeroList();
-  }
+	ngOnInit() {
+		this.getHeroList();
+	}
 	onSelectHero(hero: Hero): void {
-	  this.selectedHero = hero;
-	this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+		this.selectedHero = hero;
+		this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
 	}
 	
 	getHeroList(): void {
-	  //this.myHeroList = this.heroService.getHeroes();
 		this.heroService.getHeroes().subscribe(result => this.myHeroList = result);
 	}
 	addHero(name: string): void {
-	  name = name.trim();
-	  if (!name) { return; }
-	  this.heroService.addHero({ name } as Hero)
-	    .subscribe(hero => {
-	      this.myHeroList.push(hero);
-	    });
+		name = name.trim();
+		if (!name) { return; }
+		this.heroService.addHero({ name } as Hero)
+			.subscribe(hero => {
+				this.myHeroList.push(hero);
+			});
 	}
 	deleteHero(hero: Hero): void {
-	  this.myHeroList = this.myHeroList.filter(h => h !== hero);
-	  this.heroService.deleteHero(hero).subscribe();
+		this.myHeroList = this.myHeroList.filter(h => h !== hero);
+		this.heroService.deleteHero(hero).subscribe();
 	}
 }
